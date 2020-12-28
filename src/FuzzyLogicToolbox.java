1808909e1402a21b504f1c9354b7a3bf5be676ba
Lauren Safwat <36446574+lauren-safwat@users.noreply.github.com>
2020-12-28 17:01:55 +0200

    Fuzzification and Defuzzification methods are implemented

--- src/FuzzyLogicToolbox.java
+++ src/FuzzyLogicToolbox.java
@@ -32,8 +32,9 @@ public class FuzzyLogicToolbox {
     public void addVariable(String name, String type){
         if(type.equalsIgnoreCase("input"))
             inputVars.add(new Variable(name));
-        else
+        else {
             outputVars.add(new Variable(name));
+        }
     }
 
     public Variable getVariable(String name){
@@ -50,5 +51,50 @@ public class FuzzyLogicToolbox {
 
     public void addRule(String rule){
         rules.add(new Rule(rule));
+    }
+
+    public int getRange(float crisp, ArrayList<Point> points){
+        for (int i = 0; i < points.size()-1; i++) {
+            if(crisp <= points.get(i+1).getX())
+                return i;
+        }
+        return -1;
+    }
+
+    public float getSlope(Point p1, Point p2){
+        return ((p2.getY() - p1.getY()) / (p2.getX() - p1.getX()));
+    }
+
+    public void fuzzification(){
+        for (Variable inputVar : inputVars) {
+            for (FuzzySet fuzzySet : inputVar.getFuzzySets()) {
+                int firstIdx = getRange(inputVar.getCrispValue(), fuzzySet.getPoints());
+                if(firstIdx == -1)
+                    inputVar.getMembershipDegrees().add(0f);
+                else{
+                    Point p1 = fuzzySet.getPoints().get(firstIdx);
+                    Point p2 = fuzzySet.getPoints().get(firstIdx+1);
+                    if (p1.getX() == p2.getX())
+                        inputVar.getMembershipDegrees().add(1f);
+                    else {
+                        float m = getSlope(p1, p2);
+                        float c = p1.getY() - (m * p1.getX());
+                        inputVar.getMembershipDegrees().add((m * inputVar.getCrispValue()) + c);
+                    }
+                }
+            }
+        }
+    }
+
+    public void defuzzification(){
+        for (Variable outputVar : outputVars) {
+            float num = 0, deno = 0;
+            for (int i = 0; i < outputVar.getFuzzySets().size(); i++) {
+                outputVar.getFuzzySets().get(i).setCentroid();
+                num += (outputVar.getFuzzySets().get(i).getCentroid() * outputVar.getMembershipDegrees().get(i));
+                deno += outputVar.getMembershipDegrees().get(i);
+            }
+            outputVar.setCrispValue(num/deno);
+        }
     }
 }
